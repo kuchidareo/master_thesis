ea37524d1987d94b7a62d61c1fafeba05303789d
Farooq Ayoub Dar <aaqib4466@gmail.com>
2024-06-27 16:43:11 +0300

    Add the log files.

---
+++ fl_on_raspberryPI_tutorial/open_clients.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Array of server hosts
+SERVER_HOSTS=(
+	"192.168.0.112"
+        "192.168.0.113"
+	"192.168.0.114"
+	"192.168.0.115"
+	"192.168.0.116"
+	"192.168.0.117"
+	"192.168.0.118"
+	"192.168.0.119"
+	"192.168.0.120"
+	"192.168.0.121"	
+)
+
+# SSH details
+SSH_USER="rasheed"
+SSH_PASSWORD="modenaottun"
+
+for HOST in "${SERVER_HOSTS[@]}"; do
+  gnome-terminal --tab -- bash -c "sshpass -p '$SSH_PASSWORD' ssh $SSH_USER@$HOST; exec bash"
+done
+
