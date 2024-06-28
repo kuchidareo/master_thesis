af1ec062fcf55c7aec12594d36f248c8be3a76fc
kuchidareo <kuchidareo@gmail.com>
2024-06-28 08:42:28 +0300

    Fix open_client.

--- fl_on_raspberryPI_tutorial/open_clients.sh
+++ fl_on_raspberryPI_tutorial/open_clients.sh
@@ -3,7 +3,7 @@
 # Array of server hosts
 SERVER_HOSTS=(
 	"192.168.0.112"
-        "192.168.0.113"
+    "192.168.0.113"
 	"192.168.0.114"
 	"192.168.0.115"
 	"192.168.0.116"
@@ -20,11 +20,10 @@ SSH_PASSWORD="modenaottun"
 
 for HOST in "${SERVER_HOSTS[@]}"; do
   gnome-terminal --tab -- bash -c "
-    sshpass -p '$SSH_PASSWORD' ssh $SSH_USER@$HOST << EOF
-      cd kuchida/master_thesis/fl_on_raspberryPI_tutorial
-      source venv/bin/activate
-	  git pull
+    sshpass -p '$SSH_PASSWORD' ssh -t $SSH_USER@$HOST '
+      cd kuchida/master_thesis/fl_on_raspberryPI_tutorial && \
+      source venv/bin/activate && \
       exec bash
-	EOF
+    '
   "
 done
