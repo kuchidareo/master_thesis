44f71abbc5c62cec632a4bfae33bb127624c38c7
Farooq Ayoub Dar <aaqib4466@gmail.com>
2024-06-28 08:59:32 +0300

    Add the input shape flattend.

--- fl_on_raspberryPI_tutorial/client_pytorch.py
+++ fl_on_raspberryPI_tutorial/client_pytorch.py
@@ -54,7 +54,7 @@ class Net(nn.Module):
         self.fc3 = nn.Linear(200, 10)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        print(x.size())
+        x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = self.relu1(x)
         x = self.fc2(x)
