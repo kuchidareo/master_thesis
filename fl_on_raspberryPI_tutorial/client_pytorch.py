e8c834f9893c8eeaee57f5e402d43e58d9bfa175
kuchidareo <kuchidareo@gmail.com>
2024-06-28 08:32:13 +0300

    Degrade the model, according to the fedavg paper.

--- fl_on_raspberryPI_tutorial/client_pytorch.py
+++ fl_on_raspberryPI_tutorial/client_pytorch.py
@@ -48,20 +48,19 @@ class Net(nn.Module):
 
     def __init__(self) -> None:
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(28 * 28, 200)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(200, 200)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(200, 10)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 4 * 4)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
+        return x
 
 
 def train(net, trainloader, optimizer, epochs, device):
