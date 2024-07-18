94034ce457a6729d4e756ce9dfd5a0b053c08a2e
kuchidareo <kuchidareo@gmail.com>
2024-07-18 16:27:00 +0300

    Change the ecg model. Input size changing.

--- fl_on_raspberryPI_tutorial/client_pytorch.py
+++ fl_on_raspberryPI_tutorial/client_pytorch.py
@@ -92,6 +92,7 @@ class EcgConv1d(nn.Module):
         self.softmax = nn.Softmax(dim=1)
     
     def forward(self, x):
+        x = x.view(x.size(0), -1)
         x = self.pool(self.relu(self.conv1(x)))
         x = self.relu(self.conv2(x))
         x = self.relu(self.conv3(x))
