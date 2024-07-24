2819b54dce90e008e0585a1c89748b5b3a2e94ff
kuchidareo <kuchidareo@gmail.com>
2024-07-24 10:46:05 +0300

    Add git pull when opening shell.

--- fl_on_raspberryPI_tutorial/open_clients.sh
+++ fl_on_raspberryPI_tutorial/open_clients.sh
@@ -23,6 +23,7 @@ for HOST in "${SERVER_HOSTS[@]}"; do
     sshpass -p '$SSH_PASSWORD' ssh -t $SSH_USER@$HOST '
       cd kuchida/master_thesis/fl_on_raspberryPI_tutorial && \
       source venv/bin/activate && \
+	  git pull && \
       exec bash
     '
   "
