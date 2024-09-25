e6bdd2a68a82ff224a5a4775a2c5b34b08ddf5fc
kuchidareo <kuchidareo@gmail.com>
2024-09-25 19:01:18 +0300

    Add pil to tensor transformation.

--- fl_on_raspberryPI/data_poisoning/blurring.py
+++ fl_on_raspberryPI/data_poisoning/blurring.py
@@ -6,16 +6,17 @@ from PIL import Image, ImageFilter
 def attack(trainset, dataset_info, rate):
     X_train = trainset[dataset_info["data_key"]]
     y_train = trainset[dataset_info["label_key"]]
-    trans = torchvision.transforms.ToPILImage()
+    trans_to_pil = torchvision.transforms.ToPILImage()
+    trans_to_tensor = torchvision.transforms.PILToTensor()
 
     poisoned_count = int(len(X_train) * rate)
     random_index = np.random.choice(len(X_train), poisoned_count, replace=False)
 
     for index in random_index:
-        image = trans(X_train[index])
+        image = trans_to_pil(X_train[index])
         image_blurred = image.filter(ImageFilter.GaussianBlur(radius=2))
-        X_train[index] = np.array(image_blurred)
-    
+        X_train[index] = trans_to_tensor(image_blurred)
+
     poisoned_trainset = Dataset.from_dict({
         dataset_info["data_key"]: X_train,
         dataset_info["label_key"]: y_train
