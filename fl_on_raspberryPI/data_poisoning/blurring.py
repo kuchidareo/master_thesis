2d5790f74854ebbaa617232a798d26ecf0099f31
Farooq Ayoub Dar <aaqib4466@gmail.com>
2024-09-25 18:44:22 +0300

    Fix code of converting PIL image from torch.

--- fl_on_raspberryPI/data_poisoning/blurring.py
+++ fl_on_raspberryPI/data_poisoning/blurring.py
@@ -1,16 +1,18 @@
 from datasets import Dataset 
 import numpy as np
+import torchvision
 from PIL import Image, ImageFilter
 
 def attack(trainset, dataset_info, rate):
     X_train = trainset[dataset_info["data_key"]]
     y_train = trainset[dataset_info["label_key"]]
+    trans = torchvision.ToPILImage()
 
     poisoned_count = int(len(X_train) * rate)
     random_index = np.random.choice(len(X_train), poisoned_count, replace=False)
 
     for index in random_index:
-        image = Image.fromarray(X_train[index])
+        image = trans(X_train[index])
         image_blurred = image.filter(ImageFilter.GaussianBlur(radius=2))
         X_train[index] = np.array(image_blurred)
     
