41170b4fbec1a222e1e64401248d45e060fbd578
kuchidareo <kuchidareo@gmail.com>
2024-10-08 14:12:32 +0300

    Add pca analysis code.

--- llm_reasoning_benchmark/evaluate_sentence_similarity.py
+++ llm_reasoning_benchmark/evaluate_sentence_similarity.py
@@ -4,6 +4,7 @@ import os
 from sentence_transformers import SentenceTransformer, util
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+from openai import OpenAI
 
 from static import UCI_ex_result_annotation
 
@@ -11,33 +12,47 @@ from static import UCI_ex_result_annotation
 class SentenceSimilarity:
     pca_components = 2
 
-    def __init__(self, base_sentence, sentence_list, model_name='sentence-transformers/all-MiniLM-L6-v2'):
-        self.model = SentenceTransformer(model_name)
+    def __init__(self, base_sentence, sentence_list, model_name):
+        self.model_type = None
+        if model_name == "all-MiniLM-L6-v2":
+            self.model = SentenceTransformer(f"sentence-transformers/{model_name}")
+            self.model_type = "sentence-transformer"
+        elif model_name == "text-embedding-3-small":
+            self.model = "text-embedding-3-small"
+            self.model_type = "openai-embeddings"
+
         self.base_sentence = base_sentence
         self.sentence_list = sentence_list
         self.cosine_similarity_list = []
 
     def get_cosine_similarity(self, sentence1, sentence2):
-        embedding1 = self.model.encode(sentence1, convert_to_tensor=True)
-        embedding2 = self.model.encode(sentence2, convert_to_tensor=True)
+        if self.model_type == "sentence_transformer":
+            embedding1 = self.model.encode(sentence1, convert_to_tensor=True)
+            embedding2 = self.model.encode(sentence2, convert_to_tensor=True)
+        elif self.model_type == "openai-embeddings":
+            embedding1 = client.embeddings.create(input=[sentence1], model=self.model).data[0].embedding
+            embedding2 = client.embeddings.create(input=[sentence2], model=self.model).data[0].embedding
 
         cosine_scores = util.pytorch_cos_sim(embedding1, embedding2)
-        return cosine_scores
+        return float(cosine_scores.cpu())
 
     def get_pca_embedding(self):
         pca = PCA(n_components=self.pca_components)
-        embedding_list = [self.model.encode(sentence, convert_to_tensor=True).cpu() for sentence in self.sentence_list]  
+        if self.model_type == "sentence-transformer":
+            embedding_list = [self.model.encode(sentence, convert_to_tensor=True).cpu() for sentence in self.sentence_list]  
+        elif self.model_type == "openai-embeddings":
+            embedding_list = [client.embeddings.create(input=[sentence], model=self.model).data[0].embedding for sentence in self.sentence_list]
         pca.fit(embedding_list)
-        return pca.transform(embedding_list)
+        return pca.transform(embedding_list)        
     
     def get_cosine_similarity_list(self):
         for sentence in self.sentence_list:
             self.cosine_similarity_list.append(self.get_cosine_similarity(sentence, self.base_sentence))
         return self.cosine_similarity_list
     
-    def make_plot(self, annotation, ex_tag, model, rate, xlabel, ylabel):
+    def make_plot(self, annotation, ex_tag, model, embedding_model, rate, xlabel, ylabel):
         pca_embedding = self.get_pca_embedding()
-        title = f"{ex_tag}_{model}_{rate}"
+        title = f"{ex_tag}_{embedding_model}_{model}_{rate}"
 
         plt.scatter(0.0, 0.0, c="blue")
         plt.annotate(f'baseline', (0.0, 0.0))
@@ -68,7 +83,7 @@ def extract_sentence_list(base_sentence_path, path_list):
             lines = str(file.readlines())
             start_index = lines.find('### assistant') + 1
             end_index = lines.rfind('### user')
-            sentence_list.append(str([line.strip() for line in lines[start_index:end_index]]))
+            sentence_list.append(str(lines[start_index:end_index].strip()))
 
     return base_sentence, sentence_list
 
@@ -86,21 +101,23 @@ def get_path_list(ex_tag, model, rate):
 
 ex_tag = "story_correction" # binary_choice, story_correction, seq_story_correction
 model = "gpt-4o-2024-08-06"
+embedding_model = "all-MiniLM-L6-v2" # all-MiniLM-L6-v2, text-embedding-3-small
 rates = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
 
 base_sentence_path = os.path.join('ex_result', 'baseline_story', "S1-ADL1.dat_gpt-4o-2024-08-06_1_['locomotion']_swim_random_1_0.0_1.md")
-
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def main():
     cosine_similarity_list = defaultdict(list)
     for rate in rates: 
         path_list = get_path_list(ex_tag, model, rate)
         base_sentence, sentence_list = extract_sentence_list(base_sentence_path, path_list)
-        similarity = SentenceSimilarity(base_sentence, sentence_list)
+        similarity = SentenceSimilarity(base_sentence, sentence_list, model_name=embedding_model)
         similarity.make_plot(
             UCI_ex_result_annotation,
             ex_tag=ex_tag,
             model=model,
+            embedding_model=embedding_model,
             rate=rate,
             xlabel="PCA 1",
             ylabel="PCA 2"
